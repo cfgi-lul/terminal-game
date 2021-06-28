09c0e3a8a4333c354dd1af2a87af9aac5598aa71
rylskikh@skbkontur.ru <rylskikh@skbkontur.ru>
2021-06-28 23:51:04 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {SharedComponentsModule} from "./shared-components/shared-components.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        SharedComponentsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
